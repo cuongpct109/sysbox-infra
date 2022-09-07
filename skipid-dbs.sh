af8df2ca17a71b2655ddff11e8a28cf9d7475cee
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-07 11:48:34 +0700

    Update skipid-dbs.sh

--- skipid-dbs.sh
+++ skipid-dbs.sh
@@ -90,7 +90,7 @@ fi
 wget --load-cookies ~/Downloads/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/Downloads/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1tJJiMOaYtmzSskWq1EGgCOmR6J3VZFqs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1tJJiMOaYtmzSskWq1EGgCOmR6J3VZFqs" -O ~/Downloads/datastack.tar.gz && rm ~/Downloads/cookies.txt
 
 # Unzip datastack.tar.gz
-cd ~/Downloads && tar -xvf datastack.tar.gz && git clone https://gitlab.com/ultorex/skipid/backend/dbs.git ~/Documents/dbs && rsync -a ~/Downloads/DataStack/ ~/Documents/dbs/
+cd ~/Downloads && tar -xvf datastack.tar.gz && git clone https://gitlab.com/ultorex/skipid/backend/dbs.git ~/Documents/dbs && cp -rf ~/Downloads/DataStack/* ~/Documents
 
 # Run docker-compose and save logs to ~/Documents/dbs/docker.log
 
