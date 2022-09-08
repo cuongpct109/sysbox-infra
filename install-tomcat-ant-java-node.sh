a6e4a976cd7964271fd1334840c00b7d86f2583f
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-08 18:22:48 +0700

    Update and rename install-tomcat-ant-java.sh to install-tomcat-ant-java-node.sh

--- install-tomcat-ant-java-node.sh
+++ install-tomcat-ant-java-node.sh
@@ -96,3 +96,12 @@ if [ "$(echo $0)" = "zsh" ] || [ "$(echo $0)" = "/bin/zsh" ]; then
         source ~/.zshrc
     fi
 fi
+
+#############
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source ~/.bashrc
+nvm install --lts
