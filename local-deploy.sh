6a178cbf16b3df9bce4147915672bc3fde7a3414
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-08 21:24:24 +0700

    Update local-deploy.sh

--- local-deploy.sh
+++ local-deploy.sh
@@ -43,17 +43,9 @@ cd ~/Documents/skipid && git clone https://gitlab.com/ultorex/skipid/backend/com
 
 cd ~/Documents/skipid/kyc-api/src/main/resources && sed -i s:54.179.113.12:127.0.0.1:g application-dev.properties
 
-# Build kyc-api with gradlew then move file war into webapps folder of tomcat-8.0.53
-
-cd ~/Documents/skipid/kyc-api && ./gradlew build && cd ~/Documents/skipid/kyc-api/build/libs && mv kyc-api-0.0.1-SNAPSHOT.war ~/apache-tomcat-8.0.53/webapps/ && mv ~/apache-tomcat-8.0.53/webapps/kyc-api-0.0.1-SNAPSHOT.war ~/apache-tomcat-8.0.53/webapps/kyc-api.war
-
 # Clone kyc-admin source
 
 cd ~/Documents/skipid && git clone https://gitlab.com/ultorex/skipid/backend/kyc-admin.git && cd ~/Documents/skipid/kyc-admin/config && sed -i s:54.179.113.12:127.0.0.1:g jdbc.properties && sed -i s:54.179.113.12:127.0.0.1:g redis.properties
-
-# Build kyc-admin with ant then move war file to webapps folder of tomcat-8.0.53
-
-cd ~/Documents/skipid/kyc-admin && ~/apache-ant-1.9.16/bin/ant && cd ~/Documents/skipid/kyc-admin/dist && mv hibtc-back.war ~/apache-tomcat-8.0.53/webapps/
 
 # Clone skipid frontend and copy .env (previously add to ~/Documents/utilities folder) to skipid frontend base folder 
 
@@ -62,23 +54,6 @@ cd ~/Documents/skipid && git clone https://gitlab.com/ultorex/skipid/frontend/sk
 cd ~/Documents/skipid/skipid && touch .env && cp /home/cuong/Documents/utilities/.env ~/Documents/skipid/skipid/
 cd ~/Documents/skipid/skipid && sed -i s:'https\://staging-api.skipid.io':'http\://localhost\:8080/kyc-api':g .env
 
-# Install skipid frontend with yarn
-
-
-cd ~/Documents/skipid/skipid
-npm install 
-sudo chmod -R 777 ~/Documents/skipid/skipid/node_modules
-npm install -g serve
-
-# Start tomcat server at port 8080
-
-~/apache-tomcat-8.0.53/bin/startup.sh
-
-# Start skipid frontend server at port 3000
-
-cd ~/Documents/skipid/skipid 
-npm run build
-serve -s build -p 3000 &
 
 # Remove .env file in the utilities folder
 
