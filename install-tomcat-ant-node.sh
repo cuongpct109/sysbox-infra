45579574fbc66f2d8481151fffb000c2f054f06c
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-08 21:05:57 +0700

    Update and rename install-tomcat-ant-java-node.sh to install-tomcat-ant-node.sh

---
+++ install-tomcat-ant-node.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Download apache-tomcat-8.0.53 and apache-ant-1.9.16
+
+cd ~ && wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.53/bin/apache-tomcat-8.0.53.zip && unzip -o apache-tomcat-8.0.53.zip && rm apache-tomcat-8.0.53.zip && chmod +x ~/apache-tomcat-8.0.53/bin/catalina.sh ~/apache-tomcat-8.0.53/bin/startup.sh ~/apache-tomcat-8.0.53/bin/shutdown.sh
+cd ~ && wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.9.16-bin.zip && unzip -o apache-ant-1.9.16-bin.zip && rm apache-ant-1.9.16-bin.zip && chmod +x ~/apache-ant-1.9.16/bin/ant ~/apache-ant-1.9.16/bin/antRun
+
+
+
+# Install node
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+
+bash install_nvm.sh
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  
+nvm install --lts
+source ~/.bashrc
