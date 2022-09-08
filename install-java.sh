8ba0019d9436d43a70c6ac12dfc3f0db3efa132f
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-08 21:05:29 +0700

    Create install-java.sh

---
+++ install-java.sh
@@ -0,0 +1,29 @@
+if [ -d ~/java1.8-272 ] 
+then
+  rm -rf ~/java1.8-272
+  cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
+  tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz
+  rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz*
+  mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272
+  if [ -z "$(grep -e ':$JAVA_HOME/bin' ~/.bashrc | grep -w "export" | grep -w "PATH")" ] && [ -z "$(grep -e 'JAVA_HOME=' ~/.bashrc | grep -w "export")" ];
+  then 
+    echo 'export JAVA_HOME=~/java1.8-272' >> ~/.bashrc
+    echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc
+  else
+    :
+  fi
+  source ~/.bashrc
+else
+  cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
+  tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
+  rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz* 
+  mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272
+  if [ -z "$(grep -e ':$JAVA_HOME/bin' ~/.bashrc | grep -w "export" | grep -w "PATH")" ] && [ -z "$(grep -e 'JAVA_HOME=' ~/.bashrc | grep -w "export")" ];
+  then 
+    echo 'export JAVA_HOME=~/java1.8-272' >> ~/.bashrc
+    echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc
+  else
+    :
+  fi
+  source ~/.bashrc
+fi
