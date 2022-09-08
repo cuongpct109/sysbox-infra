1e0d18f87c71731c2dc4b7b17f71ae04b4423505
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-08 20:59:40 +0700

    Update install-tomcat-ant-java-node.sh

--- install-tomcat-ant-java-node.sh
+++ install-tomcat-ant-java-node.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Download apache-tomcat-8.0.53 and apache-ant-1.9.16
-
-cd ~ && wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.53/bin/apache-tomcat-8.0.53.zip && unzip -o apache-tomcat-8.0.53.zip && rm apache-tomcat-8.0.53.zip && chmod +x ~/apache-tomcat-8.0.53/bin/catalina.sh ~/apache-tomcat-8.0.53/bin/startup.sh ~/apache-tomcat-8.0.53/bin/shutdown.sh
-cd ~ && wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.9.16-bin.zip && unzip -o apache-ant-1.9.16-bin.zip && rm apache-ant-1.9.16-bin.zip && chmod +x ~/apache-ant-1.9.16/bin/ant ~/apache-ant-1.9.16/bin/antRun
-
 # Install java
 
 if [ "$(echo $0)" = "bash" ] || [ "$(echo $0)" = "/bin/bash" ] || [ "$(echo $0)" = "-bash" ]; then
@@ -38,6 +33,14 @@ if [ "$(echo $0)" = "bash" ] || [ "$(echo $0)" = "/bin/bash" ] || [ "$(echo $0)"
         source ~/.bashrc
     fi
 fi
+
+
+# Download apache-tomcat-8.0.53 and apache-ant-1.9.16
+
+cd ~ && wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.53/bin/apache-tomcat-8.0.53.zip && unzip -o apache-tomcat-8.0.53.zip && rm apache-tomcat-8.0.53.zip && chmod +x ~/apache-tomcat-8.0.53/bin/catalina.sh ~/apache-tomcat-8.0.53/bin/startup.sh ~/apache-tomcat-8.0.53/bin/shutdown.sh
+cd ~ && wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.9.16-bin.zip && unzip -o apache-ant-1.9.16-bin.zip && rm apache-ant-1.9.16-bin.zip && chmod +x ~/apache-ant-1.9.16/bin/ant ~/apache-ant-1.9.16/bin/antRun
+
+
 
 # Install node
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
